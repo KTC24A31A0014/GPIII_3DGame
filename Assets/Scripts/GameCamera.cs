1c1c5932beac5ee62d9a1233f90fcc4d9493dee3
KTC24A31A0014 <ktc24a31a0014@edu.kyoto-tech.ac.jp>
2026-07-03 12:39:36 +0900

    アニメーションを追加（修正が必要な箇所あり）。ジャンプを追加。

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField] PlayerInput playerInput;
+
+    [SerializeField] Transform lookTarget;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float targetDistance;
+    [SerializeField] float rotateSpeed;
+
+    private float pitch;
+    private float yaw;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        yaw = 90;
+        pitch = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lookVec = playerInput.actions["Look"].ReadValue<Vector2>();
+        yaw += lookVec.x * rotateSpeed * Time.deltaTime;
+        pitch -= lookVec.y * rotateSpeed * Time.deltaTime;
+
+        var taget = lookTarget.position + offset;
+        var rotation = Quaternion.Euler(pitch, yaw, 0);
+        var position = rotation * new Vector3(0, 0, -targetDistance) + taget;
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speedMax;
+    [SerializeField] float accel;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] float jumpSpeed;
+
+    PlayerInput playerInput;
+    Rigidbody rb;
+    Animator animator;
+    Vector3 rotateTarget;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var accelVec = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        var cameraDir = playerInput.camera.transform.forward;
+        cameraDir.y = 0;
+        cameraDir = cameraDir.normalized;
+
+        var cameraRight = playerInput.camera.transform.right;
+
+        var accelVec3D = cameraDir * accelVec.y * accel + cameraRight * accelVec.x * accel;
+
+        rb.AddForce(accelVec3D, ForceMode.Acceleration);
+
+        if (accelVec3D != Vector3.zero)
+        {
+            rotateTarget = accelVec3D.normalized;
+        }
+        // 前方向をコピーしておく
+        Vector3 forward = transform.forward;
+
+        // 上方向を固定
+        transform.up = Vector3.up;
+
+        // 前方向をターゲットに向かって補間
+        transform.forward = Vector3.Slerp(forward, rotateTarget, rotateSpeed * Time.deltaTime);
+
+        // AnimatorのMoveSpeedパラメータにRigidbodyの移動速度の大きさを与える
+        Vector3 velocityXZ = rb.linearVelocity;
+        velocityXZ.y = 0;
+        animator.SetFloat("MoveSpeed", rb.linearVelocity.magnitude);
+
+        // Jump
+        if (playerInput.actions["Jump"].WasPressedThisFrame())
+        {
+            Vector3 jumpVec = new Vector3(0, jumpSpeed, 0);
+            rb.AddForce(jumpVec, ForceMode.VelocityChange);
+        }
+    }
+}
